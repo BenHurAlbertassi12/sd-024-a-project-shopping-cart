5023de105225029a625d8112b4a01065a0603dcb
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-09 18:19:52 -0300

    Fetch Products 100% se Deus quiser

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -13,16 +13,17 @@ describe('1 - Teste a função fetchProducts', () => {
     //Exercicio 9.3 jest
   });
   it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
-    expect(fetchProducts('computador')).toEqual("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+    expect(fetchProducts('computador')).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+  });
+  //Exercicio 9.3 jest
+
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', () => {
+    expect(fetchProducts('computador')).toBe(computadorSearch)
   });
 
-  // it('', () => {
-  //   expect()
-  // });
-
-  // it('', () => {
-  //   expect()
-  // });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url".', () => {
+    expect(fetchProducts(undefined)).toBe(new Error('You must provide an url'))
+  });
 
 
 
