a1d0cad6cfba54f7b44bbbcb2c7cce61c6687f15
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-09 18:07:02 -0300

    test llint corrigido

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,14 +1,9 @@
-const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
-
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 const fetchProducts = () => {
-  // seu código aqui
-  const fetchSimulator = (url) => {
-    if (!url || url !== ValidURL)
-      return Promise(new Error('Produto Invalido'))
-  }
-
-  return fetch = url;
+  fetch(url);
+  return fetch;
 };
+// console.log(fetchProducts('computador'));
 
 if (typeof module !== 'undefined') {
   module.exports = {
