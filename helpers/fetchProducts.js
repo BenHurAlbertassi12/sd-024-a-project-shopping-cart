645f19b0455413c8f8e980e737f0303f0b4051f2
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-09 17:51:45 -0300

    teste % github

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+
 const fetchProducts = () => {
   // seu código aqui
+  const fetchSimulator = (url) => {
+    if (!url || url !== ValidURL)
+      return Promise(new Error('Produto Invalido'))
+  }
+
+  return fetch = url;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,40 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  });
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada;', () => {
+    expect(fetchProducts('computador').toHaveBeenCalled(fetch))
+    // https://jestjs.io/pt-BR/docs/expect#tohavebeencalled
+    //Exercicio 9.3 jest
+  });
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
+    expect(fetchProducts('computador')).toEqual("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+  });
+
+  // it('', () => {
+  //   expect()
+  // });
+
+  // it('', () => {
+  //   expect()
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
