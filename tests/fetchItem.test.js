19b4d7d2240be047b01645ae147b3747b487a0aa
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-09 15:14:55 -0300

    corrigindo

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,7 +1,7 @@
 require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
-alterando para commit
+
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
   fail('Teste vazio');
