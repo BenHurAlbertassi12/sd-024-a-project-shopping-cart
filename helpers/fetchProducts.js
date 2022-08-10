e87d11f2008b4f1a6e497834bc6cbeefb28541da
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-09 23:40:26 -0300

    Chega por hoje

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,26 +1,26 @@
-const fetchURL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-// const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+// const fetchURL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
 
 // sku: é o campo id retornado pela API;
 // name: é o campo title retornado pela API;
 // image: é o campo thumbnail retornado pela API.
 
+pausa por Hoje video 1h11min
+
 const fetchProducts = () => {
   // await
   // os dados estão devolvendo uma nova promessa 46min00seg
-  fetch(fetchURL)
+  fetch(endpoint)
     .then((respostaApi) => respostaApi.json())
     .then((objetoComputador) => {
       const pCs = {
-        sku: objetoComputador.reults.id,
+        sku: objetoComputador.results.id,
         name: objetoComputador.results.title,
         image: objetoComputador.results.thumbnail,
       };
-      console.log(pCs);
+      console.log(pCs('results'));
     })
-    .catch((error) => error.toString());
-  console.log(fetchURL);
-  return fetchURL;
+    .catch();
 };
 
 if (typeof module !== 'undefined') {
