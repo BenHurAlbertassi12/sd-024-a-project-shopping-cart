c1ceb6142e328a5217ec81013e58fcda39ed7c6c
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-11 11:44:26 -0300

    fetchItem passando no teste

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,14 @@
-const fetchItem = () => {
+const fetchItem = async (buscaId) => {
   // seu código aqui
+  try {
+    const endpoint = await fetch(`https://api.mercadolibre.com/items/${buscaId}`);
+    const resultado = await endpoint.json();
+    return resultado; // retirei o '.results' e passou no teste
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
 };
+// fetchItem();
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -14,21 +14,21 @@ describe('2 - Teste a função fetchItem', () => {
 
   it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
     await fetchItem('MLB1615760527')
-    expect(fetch).toEqual('https://api.mercadolibre.com/items/MLB1615760527')
-    // expect(fetch).toHaveBeenCalled('https://api.mercadolibre.com/items/MLB1615760527')
+    // expect(fetch).toEqual('https://api.mercadolibre.com/items/MLB1615760527')
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527')
   })
 
   it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
-    await fetchItem('MLB1615760527')
-    expect(fetch).toEqual()
+    // const linha21 = await fetchItem('MLB1615760527');
+    expect(await fetchItem('MLB1615760527')).toEqual(item)
   })
 
-  // it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url".', () => {
-  //   try {
-  //     await fetchItem()
-  //   }
-  //   catch (error) {
-  //     expect(error).toEqual(new Error('You must provide an url'))
-  //   }
-  //})
+  it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url".', async () => {
+    try {
+      await fetchItem()
+    }
+    catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'))
+    }
+  })
 });
