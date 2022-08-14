b1b3d062fd3e59ee3ca8a96b4b5fa81879f93497
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-14 15:33:29 -0300

    Finalizando o projeto shoppingCart

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cartItems = document.querySelector('.cart__items');
 
+// veio pronto
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -7,6 +8,7 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
+// veio pronto
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -14,6 +16,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+// veio pronto
 const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,12 +29,15 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
   return section;
 };
 
+// veio pronto
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
+// adicionado ao parametro um target.remove
 const cartItemClickListener = (event) => {
   event.target.remove();
 };
 
+// veio pronto
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -40,49 +46,54 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-const novaFun = (linha) => {
+// feito em mentoria
+const seletcCartItems = (linha) => {
   const olLinha = document.querySelector('.cart__items');
   olLinha.appendChild(linha);
 };
 
-const load = () => {
-  const loadItems = document.querySelector('.items');
-  const teste = document.createElement('p');
-  teste.className = 'loading';
-  teste.innerText = 'carregando...';
-  loadItems.appendChild(teste);
-  console.log(loadItems);
+const loading = () => {
+  const loadItems = document.querySelector('.items'); // esta pegando a classe .items dentro de index.html colocando dentro de loadItems
+  const elementoP = document.createElement('p'); // Criando a tag P
+  elementoP.className = 'loading'; // adicionando a parava loading como classe da tag p
+  elementoP.innerText = 'carregando...'; // adicionando a palavra carregando para a tag p
+  loadItems.appendChild(elementoP); // acrescentadno a tag p como filho da classe .items
+};
+
+const removeLoading = () => {
+  const tagP = document.querySelector('.loading'); // pegando a classe .loading
+  tagP.remove(); // remove a tag p
 };
 
 const chamadaItem = async (busacaId) => {
-  load();
   const resBusca = await fetchItem(busacaId);
   const linha = createCartItemElement({
     sku: resBusca.id,
     name: resBusca.title,
     salePrice: resBusca.price,
   });
-  // saveCartItems(cartItems.innerHTML, totalPrice.innerHTML);
-
-  novaFun(linha);
+  seletcCartItems(linha);
 };
 
 const limpaCarro = () => {
-  const test = document.querySelector('.empty-cart');
-  test.addEventListener('click', () => {
-    cartItems.innerHTML = '';
+  const clearCar = document.querySelector('.empty-cart'); // seleciona a classe .empty-cart
+  clearCar.addEventListener('click', () => { // adicina um envento de escuta com 'click' e uma função
+    cartItems.innerHTML = ''; // escreve uma string vazia a classe quando clicada
   });
 };
 
+// feita em mentoria
 const chamadaEnviar = async (event) => {
   const skull = getSkuFromProductItem(event.target.parentElement);
   await chamadaItem(skull);
 };
+
 const chamadaComputador = async () => {
+  loading(); // chamando a função loading
   const resBusca = await fetchProducts('computador'); // Var resBusca para receber o resultado da busca sobre o prametro 'computador'
-  const filhoResult = document.querySelector('.items'); // selecionando a pasta .items
-  resBusca.forEach((busca) => { // dois dias tentando resolver, retirei o '.results' e resolveu
-    // refino da busca com o forEach para pegar apenas o results
+  removeLoading();// chamando a função removeLoading
+  const filhoResult = document.querySelector('.items'); // selecionando a classe .items
+  resBusca.forEach((busca) => { // dois dias tentando resolver, retirei o '.results' e resolveu.// refino da busca com o forEach para pegar apenas o que foi buscado
     const acrescenta = createProductItemElement(busca);
     // console.log(acrescenta)
     acrescenta.addEventListener('click', chamadaEnviar);// Var acrescentar (append) para trazer o que foi buscado pelo que foi declarado na linha 17
@@ -90,21 +101,12 @@ const chamadaComputador = async () => {
     // teste.push(acrescenta);
     // saveCartItems(teste);
   }); // codigo resolvido em conjunto com Cris, Uriel Silva e Lorena, sala de estudos numero 10
+  // duas horas na mentoria com o Bruno, o coitado esta morto de cansado de mim, sendo honesto, sem ele eu não teria conseguido, toda a parte de adicionar o item ao carrinho foi feita em mentoria, sou extremamente grato a ele.
 };
-const getLocalStorage = () => {
-  const teste = getSavedCartItems();
-  console.log(teste);
-  const list = document.querySelectorAll('li');
-  list.forEach((li) => {
-    li.addEventListener('click', cartItemClickListener);
-  });
-};
-// duas horas na mentoria com o Bruno, o coitado esta morto de cansado de mim, sendo honesto, sem ele eu não teria conseguido, toda a parte de adicionar o item ao carrinho foi feita em mentoria, sou extremamente grato a ele.
-
 
 window.onload = () => {
   limpaCarro();
-  getLocalStorage();
+
   chamadaComputador();
   saveCartItems();
 };
